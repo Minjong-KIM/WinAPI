56fafde81d8575cb262fe792c714f180cb292032
Minjong-KIM <dure7108@naver.com>
2021-11-25 10:31:45 +0900

    first commit

---
+++ Third.cpp
@@ -0,0 +1,61 @@
+#include <Windows.h>
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
+
+int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE, LPSTR, int) {
+    WNDCLASSEXA win = { 0 };
+    win.cbSize = sizeof(win);
+    win.style = CS_HREDRAW | CS_VREDRAW;
+    win.hInstance = hinstance;
+    win.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+    win.hCursor = LoadCursor(NULL, IDC_ARROW);
+    win.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    win.lpszClassName = "my window";
+    win.lpszMenuName = NULL;
+    win.lpfnWndProc = WndProc;
+
+    RegisterClassExA(&win);
+
+    HWND hwnd;
+    hwnd = CreateWindow(
+        L"my window",
+        L"TextOut",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        0, 0, hinstance, 0
+    );
+
+
+    ShowWindow(hwnd, SW_SHOW);
+
+    MSG msg;
+    while (GetMessage(&msg, 0, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessageA(&msg);
+    }
+
+    return msg.wParam;
+}
+
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
+
+    HDC hdc;
+    PAINTSTRUCT ps;
+
+    switch (msg)
+    {
+    case WM_PAINT:
+        hdc = BeginPaint(hwnd, &ps);
+        TextOut(hdc, 100, 100, L"Hello World", 11);
+        EndPaint(hwnd, &ps);
+        return 0;
+
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    }
+    return DefWindowProcA(hwnd, msg, wparam, lparam);
+}
